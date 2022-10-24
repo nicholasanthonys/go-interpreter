e3beba51b6193f170fa78bab0ddc8ce43cfd88d7
Nicholas Anthony S <anthony.nicholas84@gmail.com>
2022-10-24 20:58:50 +0700

    add support for more operator

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -51,7 +51,14 @@ func TestNextToken_2(t *testing.T) {
 	x + y;
 	};
 	let result = add(five, ten);
-	`
+	!-/*5;
+	5 < 10 > 5;
+
+	if (5 < 10) {
+		return true;
+	} else {
+		return false;
+	}`
 
 	tests := []struct {
 		expectedType    token.TokenType
@@ -93,6 +100,20 @@ func TestNextToken_2(t *testing.T) {
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
+
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+
 		{token.EOF, ""},
 	}
 
